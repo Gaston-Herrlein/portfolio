5e24fd1f3a9ceca884b640e684778ee905d183eb
Gaston Herrlein <herrleingaston@gmail.com>
2025-09-23 17:41:09 +0200

    feat: the footer has been separated from the main component.
    NOTE: the favicon, social media links, and HTML title have been corrected, and unnecessary styles have been removed.

---
+++ src/components/footer.tsx
@@ -0,0 +1,25 @@
+import { Github, Linkedin, Twitter } from "lucide-react";
+
+const Footer = () => {
+  return (
+    <footer className="site-footer">
+      <div>© 2025 Gastón Herrlein</div>
+      <div className="socials">
+        <a
+          href="https://linkedin.com/in/gaston-herrlein"
+          className="social-link"
+        >
+          <Linkedin size={16} /> LinkedIn
+        </a>
+        <a href="https://github.com/Gaston-Herrlein" className="social-link">
+          <Github size={16} /> GitHub
+        </a>
+        <a href="https://twitter.com" className="social-link">
+          <Twitter size={16} /> Twitter
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
